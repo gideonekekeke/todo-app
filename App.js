67f266aec566d902faa208327852a1235e0c248b
Gideon Ekeke <71523437+gideonekekeke@users.noreply.github.com>
2020-11-23 00:32:42 +0100

    Add files via upload
    main app

---
+++ App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Head from './Heade/Head';
+import ViewingTask from './ViewingTask/ViewingTask';
+import HeaderFile from './MainComponent/HeaderFile';
+
+
+
+
+
+
+
+function App() {
+  return (
+  
+    <div>
+      
+      <Head/>
+     <ViewingTask/>
+ 
+     
+         {/* <HeaderFile/> */}
+
+       
+
+       </div>
+
+
+
+
+
+
+
+
+
+  
+  );
+}
+
+export default App;
